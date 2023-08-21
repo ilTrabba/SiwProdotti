e14197e86ad70bfa3c0d48368771a74217aee9a0
ilTrabba <cri.traballoni@gmail.com>
2023-08-21 11:57:14 +0200

    model

---
+++ src/main/java/it/uniroma3/siw/model/Product.java
@@ -0,0 +1,107 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private Float price;
+
+    @Column(length = 200)
+    @Length(max = 300)
+    private String description;
+
+    @OneToOne
+    Image foto;
+
+
+    @ManyToMany
+    private Set<Supplier> suppliers;
+
+    @OneToMany (mappedBy = "reviewedProduct",cascade = CascadeType.ALL)
+    private Set<Review> reviews;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Set<Supplier> getSuppliers() {
+        return suppliers;
+    }
+
+    public void setSuppliers(Set<Supplier> suppliers) {
+        this.suppliers = suppliers;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Image getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Image foto) {
+        this.foto = foto;
+    }
+
+    public Set<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(Set<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
---
+++ src/main/java/it/uniroma3/siw/model/Supplier.java
@@ -0,0 +1,92 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String address;
+
+    @NotBlank
+    private String email;
+
+    @OneToOne
+    private Image logo;
+
+    @ManyToMany(mappedBy="suppliers",fetch = FetchType.LAZY)
+    private Set<Product> starredProducts;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Product> getStarredProducts() {
+        return starredProducts;
+    }
+    public void setStarredProducts(Set<Product> starredProducts) {
+        this.starredProducts = starredProducts;
+    }
+
+    public Image getLogo() {
+        return logo;
+    }
+
+    public void setLogo(Image logo) {
+        this.logo = logo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Supplier supplier = (Supplier) o;
+        return Objects.equals(name, supplier.name) && Objects.equals(address, supplier.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address, email);
+    }
+}
