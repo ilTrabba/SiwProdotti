a77a502ea5c4eb5ad6023fef5a7f88bc7da62b1b
ilTrabba <cri.traballoni@gmail.com>
2023-08-21 19:12:47 +0200

    alcuni template html e una svista in ProductController

--- src/main/java/it/uniroma3/siw/controller/ProductController.java
+++ src/main/java/it/uniroma3/siw/controller/ProductController.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @Controller
-public class PlayerController {
+public class ProductController {
     @Autowired
     private ProductRepository productRepository;
     @Autowired
